444c46851c61747fc3c83c727c143d461c174325
hanifcoder <38731771+hanifcoder@users.noreply.github.com>
2023-01-21 09:18:38 -0500

    Web Dev at Udacity
    There you got it!

---
+++ yo.js
@@ -0,0 +1,5 @@
+function alertUser() {
+  alert(
+    "Hello scholars! Keep working hard to pass your front end challenge on February 20 before signing up for the front end web dev program!"
+  );
+}
